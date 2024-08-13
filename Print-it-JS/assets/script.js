e0f0fb4a1aab135d52a6ec05914284ac3f4d160c
Jean-Michel LE COUVIOUR <jeanmilc45@gmail.com>
2024-08-13 17:44:41 +0200

    modif JS : Fonction slide dynamique

--- Print-it-JS/assets/script.js
+++ Print-it-JS/assets/script.js
@@ -1,58 +1,67 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+    {
+        "image": "slide1.jpg",
+        "tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
+    },
+    {
+        "image": "slide2.jpg",
+        "tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+    },
+    {
+        "image": "slide3.jpg",
+        "tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+    },
+    {
+        "image": "slide4.png",
+        "tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
+    }
+];
 
 // Sélection des éléments flèche gauche et flèche droite :
 const arrowLeft = document.querySelector('#banner .arrow_left');
 const arrowRight = document.querySelector('#banner .arrow_right');
 
-// Ajout des event listeners aux clics sur flèches
-arrowLeft.addEventListener('click', () => {
-    console.log('CLIC Flèche gauche');
-});
-
-arrowRight.addEventListener('click', () => {
-    console.log('CLIC Flèche droite');
-});
-
-// Récupération du conteneur des points
+// Récupération des éléments de la bannière
+const bannerImg = document.querySelector('#banner .banner-img');
+const bannerText = document.querySelector('#banner p');
 const conteneurBulletPoints = document.querySelector('.dots');
 
+// Initialisation de l'index du slide courant
+let index = 0;
 
+// Fonction pour affichage des bullet points
 function affichBulletPoints() {
-	let nombreSlides; // Variable globale stockage nb slides
-	function compterSlides() {
-		for (let i = 0; i <= slides.length; i++) {
-			nombreSlides = i;
-		}
-		console.log(nombreSlides)
-	}
-	function placerPoints() {
-		for (let i = 0; i < nombreSlides; i++) {
-			const dot = document.createElement('div');
-			dot.classList.add('dot');
-			if (i === 0) {
-				dot.classList.add('dot_selected');  // Sélectionne le premier point
-			}
-			conteneurBulletPoints.appendChild(dot);
-		}
-	}
-    compterSlides();
-    placerPoints();
+    slides.forEach((slide, index) => {
+        const dot = document.createElement('div');
+        dot.classList.add('dot');
+        if (index === 0) {
+            dot.classList.add('dot_selected');
+        }
+        conteneurBulletPoints.appendChild(dot);
+    });
 }
 affichBulletPoints();
+
+// Fonction pour rendre le slide dynamique:
+
+function màjSlide(index) {
+    bannerImg.src = `./assets/images/slideshow/${slides[index].image}`;
+    bannerText.innerHTML = slides[index].tagLine;
+    const dots = document.querySelectorAll('.dot');
+    dots.forEach(dot => dot.classList.remove('dot_selected'));
+    dots[index].classList.add('dot_selected');
+}
+// Gestion evenement : Clic flêche gauche :
+arrowLeft.addEventListener('click', () => {
+    index --;
+	if (index <0)
+		index =0;
+    màjSlide(index);
+});
+// Gestion evenement : Clic flêche droite :
+arrowRight.addEventListener('click', () => {
+    index ++;
+	if (index >slides.length-1)
+		index =slides.length-1;
+	màjSlide(index);
+});
